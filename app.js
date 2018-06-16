088bc2feb031717521318401055d56339cb200fe
Oleksandr Pronin <oleksandr.pronin@adviqo.com>
2018-06-16 22:53:30 +0200

    Console prompt implemented

--- app.js
+++ app.js
@@ -2,33 +2,64 @@
 
 var prompt = require('prompt');
 
-prompt.start();
-
-var length = {
+var lengthPrompt = {
   name: 'length',
+  type: 'integer',
   message: 'amount?',
   validator: /[1-9]\d*|0\d+/,
   warning: 'Must respond only numbers',
   default: '230'
 };
 
-prompt.get(length, function(err, result) {
+var lowPrompt = {
+  name: 'low',
+  type: 'integer',
+  message: 'low?',
+  validator: /^[\-\+]\s*\d+\s*$/,
+  warning: 'Must respond only numbers with + or -',
+  default: '-10000'
+};
+
+var highPrompt = {
+  name: 'high',
+  type: 'integer',
+  message: 'high?',
+  validator: /^[\-\+]\s*\d+\s*$/,
+  warning: 'Must respond only numbers with + or -',
+  default: '+10000'
+};
+
+var length = 0;
+var low = 0;
+var high = 0;
+
+prompt.start();
+
+prompt.get([lengthPrompt, lowPrompt, highPrompt], function(err, result) {
   if (err == null) {
-    console.log(result);
+    length = result.length;
+    low = result.low;
+    high = result.high;
+    generateRandom(length, low, high);
+    // console.log('Input received:');
+    // console.log(JSON.stringify(result, null, 2));
   };
 });
 
-// var numbers = new Array(length);
-// for (var i = 0; i < numbers.length; i++) {
-//     numbers[i] = randomIntInc(-1000,1000) * 10
-// }
-
-// for (var i = 0; i < length; i++) {
-//   let number = numbers[i];
-//   let numberString = number.toString()
-//   console.log(numberString)
-// }
-
+function generateRandom (length, low, high) {
+  var numbers = new Array(length);
+  
+  for (var i = 0; i < numbers.length; i++) {
+    let randomValue = randomIntInc(low / 10, high / 10) * 10;
+    numbers[i] = randomValue;
+  }
+  
+  for (var i = 0; i < length; i++) {
+    let number = numbers[i];
+    let numberString = number.toString()
+    console.log(numberString)
+  }
+}
 
 function random (low, high) {
   return Math.random() * (high - low) + low;
